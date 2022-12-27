e7ea4c7aa416a080a8b1e2fddacd27d529bd68f0
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-27 21:11:00 +0100

    Update

---
+++ premake5.lua
@@ -0,0 +1,14 @@
+include "../../premake/common_premake_defines.lua"
+
+project "entt"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./src/**.h",
+		"./src/**.hpp"
+	}
